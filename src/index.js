a476ca5524066915d995481e04897a77284b5aed
carolina semprini <carolinasemprini@gmail.com>
2023-05-16 01:17:52 -0300

    puerto 8080

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import CartRouter from "./router/carts.routes.js"
 
 
 const app= express()
-const PORT=4000
+const PORT=8080
 
 app.use(express.json())
 app.use (express.urlencoded({extended:true}));
